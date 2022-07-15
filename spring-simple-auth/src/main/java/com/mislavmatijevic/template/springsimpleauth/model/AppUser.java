b22b02076800de485f482b19dde55e7f71dc8e15
Mislav Matijević <mislav.mim@gmail.com>
2022-07-15 22:50:02 +0200

    Added User model. This model is a basic model to hold user data for future registers and logins.

---
+++ spring-simple-auth/src/main/java/com/mislavmatijevic/template/springsimpleauth/model/AppUser.java
@@ -0,0 +1,70 @@
+package com.mislavmatijevic.template.springsimpleauth.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "APP_USERS")
+public class AppUser
+{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long userId;
+
+    private String firstName;
+
+    private String lastName;
+
+    private String email;
+
+    private String password;
+
+    public long getUserId()
+    {
+        return userId;
+    }
+
+    public void setUserId(final long userId)
+    {
+        this.userId = userId;
+    }
+
+    public String getFirstName()
+    {
+        return firstName;
+    }
+
+    public void setFirstName(final String firstName)
+    {
+        this.firstName = firstName;
+    }
+
+    public String getLastName()
+    {
+        return lastName;
+    }
+
+    public void setLastName(final String lastName)
+    {
+        this.lastName = lastName;
+    }
+
+    public String getEmail()
+    {
+        return email;
+    }
+
+    public void setEmail(final String email)
+    {
+        this.email = email;
+    }
+
+    public String getPassword()
+    {
+        return password;
+    }
+
+    public void setPassword(final String readablePassword)
+    {
+        this.password = readablePassword;
+    }
+}
