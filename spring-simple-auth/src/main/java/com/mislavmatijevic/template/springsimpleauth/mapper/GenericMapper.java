eeff110a86b93cf7c17b28cf1db03cb408d2e0e3
Mislav Matijević <mislav.mim@gmail.com>
2022-07-16 00:46:09 +0200

    Added new user DTO class. To be used when registering. Also added generic mapper and actual AppUser mapper for service to use.

---
+++ spring-simple-auth/src/main/java/com/mislavmatijevic/template/springsimpleauth/mapper/GenericMapper.java
@@ -0,0 +1,15 @@
+package com.mislavmatijevic.template.springsimpleauth.mapper;
+
+/**
+ * Idea based on sassa400's: https://github.com/sassa400/WebshopBasics
+ * This generic mapper allows users to implement all kinds of DTO-to-entity mappers.
+ *
+ * @param <D> Data Transfer Object (DTO) object.
+ * @param <E> Entity (persistence) object.
+ */
+public interface GenericMapper<D, E>
+{
+    D mapDto(E entity);
+
+    E map(D dto);
+}
