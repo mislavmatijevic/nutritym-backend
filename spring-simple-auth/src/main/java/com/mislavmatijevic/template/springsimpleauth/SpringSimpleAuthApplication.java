bdf7d59fc95e1895ca06cdf84e6726595c48596f
Mislav Matijević <mislav.mim@gmail.com>
2022-07-15 21:58:03 +0200

    Added template project files. Used Spring Initializr in IntelliJ.

---
+++ spring-simple-auth/src/main/java/com/mislavmatijevic/template/springsimpleauth/SpringSimpleAuthApplication.java
@@ -0,0 +1,15 @@
+package com.mislavmatijevic.template.springsimpleauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSimpleAuthApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(SpringSimpleAuthApplication.class, args);
+    }
+
+}
---
+++ spring-simple-auth/src/test/java/com/mislavmatijevic/template/springsimpleauth/SpringSimpleAuthApplicationTests.java
@@ -0,0 +1,15 @@
+package com.mislavmatijevic.template.springsimpleauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSimpleAuthApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
