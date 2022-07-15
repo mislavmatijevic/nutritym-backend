d664df660515d854e2c18d7d566eecf249938683
Mislav Matijević <mislav.mim@gmail.com>
2022-07-16 01:47:28 +0200

    Added tests for two login scenarios. Method "giveRandomUser" now returns a user with different email on every call.

--- spring-simple-auth/src/test/java/com/mislavmatijevic/template/springsimpleauth/SpringSimpleAuthApplicationTests.java
+++ spring-simple-auth/src/test/java/com/mislavmatijevic/template/springsimpleauth/SpringSimpleAuthApplicationTests.java
@@ -2,11 +2,11 @@ package com.mislavmatijevic.template.springsimpleauth;
 
 import com.mislavmatijevic.template.springsimpleauth.dto.AppUserRegisterDto;
 import com.mislavmatijevic.template.springsimpleauth.exceptions.UserAlreadyExistsException;
+import com.mislavmatijevic.template.springsimpleauth.exceptions.UserNotFoundException;
 import com.mislavmatijevic.template.springsimpleauth.mapper.AppUserRegisterMapper;
 import com.mislavmatijevic.template.springsimpleauth.model.AppUser;
 import com.mislavmatijevic.template.springsimpleauth.services.AppUserService;
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -18,9 +18,11 @@ import java.util.Optional;
 @AutoConfigureMockMvc
 class SpringSimpleAuthApplicationTests
 {
+    // Ensures random users have different emails. That makes them unique between tests.
+    private static int userCount = 0;
+
     @Autowired
     AppUserService userService;
-
     AppUserRegisterMapper userMapper = new AppUserRegisterMapper();
 
     @Test
@@ -44,12 +46,35 @@ class SpringSimpleAuthApplicationTests
         Assertions.assertThrows(UserAlreadyExistsException.class, () -> userService.register(userWithAlreadyUsedEmail));
     }
 
-    private AppUserRegisterDto giveRandomUser()
+    @Test
+    void correctLoginTest()
+    {
+        AppUserRegisterDto registerUser = giveRandomUser();
+        userService.register(userMapper.map(registerUser));
+
+        AppUser loginUser = new AppUser();
+        loginUser.setEmail(registerUser.getEmail());
+        loginUser.setPassword(registerUser.getPassword());
+
+        Assertions.assertDoesNotThrow(() -> userService.login(loginUser));
+    }
+
+    @Test
+    void badLoginTest()
+    {
+        AppUser loginUser = new AppUser();
+        loginUser.setEmail("thisEmailDoesNotExistInTheDB@tooBad.nope");
+        loginUser.setPassword("iCanLiveWithThat");
+
+        Assertions.assertThrows(UserNotFoundException.class, () -> userService.login(loginUser));
+    }
+
+    AppUserRegisterDto giveRandomUser()
     {
         AppUserRegisterDto registerUser = new AppUserRegisterDto();
         registerUser.setFirstName("Mislav");
         registerUser.setLastName("Matijević");
-        registerUser.setEmail("mislav.mim@gmail.com");
+        registerUser.setEmail("mislav.mim" + (++userCount) + "@gmail.com");
         registerUser.setPassword("abcdefgh1");
         return registerUser;
     }
