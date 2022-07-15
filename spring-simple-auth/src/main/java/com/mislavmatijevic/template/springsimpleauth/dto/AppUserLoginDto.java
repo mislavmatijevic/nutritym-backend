2b4dc86121d1b6e1c17ce3af96239e552f175cf4
Mislav Matijević <mislav.mim@gmail.com>
2022-07-16 01:17:38 +0200

    Changed hierarchy of user classes. Added a login class for a user that's logging in. It only contains password and email. Class for registering a new user extends the login class.

---
+++ spring-simple-auth/src/main/java/com/mislavmatijevic/template/springsimpleauth/dto/AppUserLoginDto.java
@@ -0,0 +1,31 @@
+package com.mislavmatijevic.template.springsimpleauth.dto;
+
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
+public class AppUserLoginDto
+{
+    private String email;
+    private String password;
+
+    public String getEmail()
+    {
+        return email;
+    }
+
+    public void setEmail(final String email)
+    {
+        this.email = email;
+    }
+
+    public String getPassword()
+    {
+        return password;
+    }
+
+    public void setPassword(final String password)
+    {
+        this.password = password;
+    }
+}
--- spring-simple-auth/src/main/java/com/mislavmatijevic/template/springsimpleauth/dto/AppUserRegisterDto.java
+++ spring-simple-auth/src/main/java/com/mislavmatijevic/template/springsimpleauth/dto/AppUserRegisterDto.java
@@ -4,12 +4,10 @@ import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
 
 @JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
-public class AppUserRegisterDto
+public class AppUserRegisterDto extends AppUserLoginDto
 {
     private String firstName;
     private String lastName;
-    private String email;
-    private String password;
 
     public String getFirstName()
     {
@@ -29,25 +27,5 @@ public class AppUserRegisterDto
     public void setLastName(final String lastName)
     {
         this.lastName = lastName;
-    }
-
-    public String getEmail()
-    {
-        return email;
-    }
-
-    public void setEmail(final String email)
-    {
-        this.email = email;
-    }
-
-    public String getPassword()
-    {
-        return password;
-    }
-
-    public void setPassword(final String password)
-    {
-        this.password = password;
     }
 }
