950a31a51c2d098e57f85dc57aa0bc99827a3101
Mislav Matijević <mislav.mim@gmail.com>
2022-07-15 22:54:34 +0200

    Added Repository Jpa interface for AppUsers.

---
+++ spring-simple-auth/src/main/java/com/mislavmatijevic/template/springsimpleauth/repository/AppUserRepository.java
@@ -0,0 +1,8 @@
+package com.mislavmatijevic.template.springsimpleauth.repository;
+
+import com.mislavmatijevic.template.springsimpleauth.model.AppUser;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AppUserRepository extends JpaRepository<AppUser, Long> { }
